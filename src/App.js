acded6963067ae49a1be2a10040446dac3002610
Puja Chaudhury <61319491+catplotlib@users.noreply.github.com>
2023-11-20 11:42:23 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import React from "react";
-import Home from "./layout/Home";
-import { Flex } from "@chakra-ui/react";
+import React, { useState, useEffect } from 'react';
+import Home from './layout/Home';
+import { Flex, Spinner, Center } from '@chakra-ui/react';
+
 function App() {
+  const [isLoading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 5000);
+  }, []);
+
+  if (isLoading) {
+    return (
+      <Center height="100vh">
+        <Spinner size="xl" />
+      </Center>
+    ); // Spinner centered on the screen
+  }
+
   return (
     <Flex bgColor="#1C1048" minHeight="100vh">
       <Home />
